f4217949344c6ba8032d8914353f7dd32dadc1e3
Irfan <irfan7861@live.com>
2020-12-22 11:54:54 +0530

    update

---
+++ Python_folder/Python basic/Logical Operator.py
@@ -0,0 +1,69 @@
+# types of operators
+# arithmetic operator
+# assihnment operator
+# comparison operator
+# logical operator
+# identically operator
+# membership operator
+# bitwise operator
+
+#arithmetic operator
+a=7
+b=8
+print(a+b)
+print(a*b)
+print(a-b)
+print(a/b)
+print(a//b)
+print(a**b)
+print(a%b)
+
+#assignment oprators
+x=7
+print(x)
+x +=7   # x = x+7
+print(x)
+
+# comparison oprators
+x=8
+print(x==8)
+print(x!=8)
+print(x>=8)
+
+#logical operators
+a= True   # 1
+b= False    #0
+print(a and b)  #1*0 = 0
+print(a and a)
+print(a or b)   # 1 + 0 =1
+print(a is not b) #!=
+
+#Membership operators
+list = [34,23,23,12,35,67]
+
+str = "abcdef"
+print('r' in str)
+print(34 in list)
+print(342 in list)
+
+# bitwise operators
+
+#dictionary
+mydict={
+     "fast": "In a quick manner",
+     "irfan": "A engineer",
+     "marks": [1,3,5,7,8,9]
+}
+print(mydict['fast'])
+print(mydict['irfan'])
+print(mydict['marks'])
+mydict['marks']=[34,56,56],
+print(mydict['marks'])
+
+
+
+
+
+
+
+
