66d0ad024813c7ba609dd533be81f4057efb6134
Irfan <irfan7861@live.com>
2020-12-24 20:05:44 +0530

    add dictionary

---
+++ Python_folder/Python basic/dictionary.py
@@ -0,0 +1,31 @@
+#dictionary is nothing but key value pairs
+#
+# d={}
+# # print(type(d))
+# d2= {"irfan":"burger", "shahir":"fish", "gourav":"roti"}
+
+# print(d2)
+#
+# print(d2["shahir"])
+# add in dictionary
+
+# d2["ankit"]= "junk food"
+# d2["amir"]= "kabab"
+# # print(d2)
+#
+#
+# # Delete in dictionary
+
+# del d2["amir"]
+# print(d2)
+
+# Copy in dictionary
+
+d2= {"irfan":"burger", "shahir":"fish", "gourav":"roti"}
+# print(d2.copy())
+# Update in dictionary
+
+
+d2.update({"rahul":"maggie"})
+print(d2)
+
